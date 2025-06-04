709976394ae1504386b1064651549a71968a123e
lauracast000009 <laracatagomez46@gmail.com>
2025-06-04 05:35:38 -0500

    🔐 Usar variables de entorno en lugar del archivo json

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,9 +1,12 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./appchatbot-90dbd-firebase-adminsdk-fbsvc-5c9d79e727.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://appchatbot-90dbd.firebaseio.com'
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL, // opcional
 });
 
 module.exports = admin;
